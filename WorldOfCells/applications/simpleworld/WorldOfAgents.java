cec59e7771f766771d76c4fa7b6a2760201c84f5
sim-cell <80094693+sim-cell@users.noreply.github.com>
2022-02-10 12:16:10 +0100

    Add files via upload

---
+++ WorldOfCells/applications/simpleworld/WorldOfAgents.java
@@ -0,0 +1,18 @@
+/*import java.util.ArrayList;
+
+public class WorldOfAgents extends World{
+	private ArrayList<Human> humans;
+	private ArrayList<Zombie> zombies;
+	private ArrayList<Animal> animals;
+	protected AgentsCA cellularAutomata;
+	
+	public WorldOfAgents(){
+		humans=new ArrayList<Human>();
+		zombies=new ArrayList<Zombie>()	;
+		animals=new ArrayList<Animal>()	;
+	}
+	
+	/*protected void stepAgents(){
+		
+	}
+}*/
