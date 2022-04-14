fb00d26867f475c1a7a0ca7aa07e7b88f440aaf1
sim-cell <celiksimayy@gmail.com>
2022-04-14 09:12:57 +0200

    Update isoworld.py

--- isoworld/isoworld.py
+++ isoworld/isoworld.py
@@ -893,7 +893,7 @@ class Food(RandDropAgents):
             if random() < PROBDROPFOOD:
                 if it%DROPDAYFOOD==0 :
                     for i in range(0, randint(5, 10)):
-                        if len(list)+i>= MAXFOOD :
+                        if len(list)>= MAXFOOD :
                             break
                         list.append(Food())
         return
@@ -911,7 +911,7 @@ class Gun(RandDropAgents) :
             if random() < PROBDROPGUN:
                 if it%DROPDAYGUN == 0 :
                     for i in range(0,randint(5, 10)):
-                        if len(list)+i>= MAXGUN :
+                        if len(list)>= MAXGUN :
                             break
                         list.append(Gun())
         return
